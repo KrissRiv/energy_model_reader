ae5def19e6cf3cc75ec35f3e4effdbcdd8900fa5
Cristian Rivas Buitrago <42716697+KrissRiv@users.noreply.github.com>
2024-12-09 20:50:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,29 +12,23 @@ def predict():
     try:
         # Get data from the request
         data = request.get_json()
-        print(f"Data: {data}")
 
         # Check if the necessary features are present
         required_features = ['coal_consumption', 'gas_consumption', 'nuclear_consumption', 'oil_consumption', 'renewables_consumption']
-        print(f" Required: {required_features}")
         if not all(feature in data for feature in required_features):
-            print("error")
             return jsonify({'error': 'Missing required features'}), 400
 
         # Create a DataFrame from the input data
         import pandas as pd
         input_df = pd.DataFrame([data])
-        print(f"Input_df {input_df}")
 
         # Make predictions
-        print(f"loaded_model {loaded_model}")
         prediction = loaded_model.predict(input_df)
 
         # Return the prediction
         return jsonify({'prediction': prediction.tolist()})
 
     except Exception as e:
-        print(f"Error: {e}")
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
